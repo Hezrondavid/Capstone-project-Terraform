4a319df3f7f5d2232c9c3f096715944315ef36d1
Hezrondavid <hezrondavid946@gmail.com>
2025-10-07 17:42:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_security_group" "devops_sg" {
@@ -50,9 +50,9 @@ resource "aws_security_group" "devops_sg" {
 }
 
 resource "aws_instance" "devops_ec2" {
-  ami           = "ami-0c101f26f147fa7fd" # Amazon Linux 2023 (us-east-1)
-  instance_type = "t2.medium"
-  key_name      = "hezron25jul2025"
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = var.instance_type
+  key_name      = var.key_name
   security_groups = [aws_security_group.devops_sg.name]
 
   tags = {
